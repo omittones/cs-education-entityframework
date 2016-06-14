27860a36bc1874159801e7d2dc95f740af2a0a4f
Augustin Juricic <ajuricic@neogov.net>
2016-06-14 18:35:36 +0200

    Added ApiExplorer example.

--- Zoo.API/Controllers/RootController.cs
+++ Zoo.API/Controllers/RootController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace Zoo.API.Controllers
 {
@@ -15,6 +18,22 @@ namespace Zoo.API.Controllers
                 minor = 1,
                 build = "*"
             });
+        }
+
+        [HttpGet]
+        [Route("docs")]
+        public IHttpActionResult Docs()
+        {
+            var endpoints = new List<object>();
+            var explorer = this.Configuration.Services.GetApiExplorer();
+            foreach (var route in explorer.ApiDescriptions)
+                endpoints.Add(new
+                {
+                    route.RelativePath,
+                    route.HttpMethod.Method
+                });
+
+            return Ok(endpoints);
         }
     }
 }
